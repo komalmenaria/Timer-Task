1945f5a6916ca0797441ad1da0bf22895f1dfec2
tonudon86 <tonudon86@gmail.com>
2022-10-11 11:23:35 +0530

    merge

--- react-timer-task/src/Components/Task.js
+++ react-timer-task/src/Components/Task.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
-const Task = () => {
+const Task = ({list,updateList}) => {
+const  handleDelete=(id)=>{
+  let mockArr=list
+  const newArray = [];
+
+  for ( let i = 0; i < mockArr.length; i++) {
+      if(i !== id) {
+        newArray.push(mockArr[i]);
+      }
+  }
+  updateList(newArray)
+  }
+  console.log(list)
   return (
-    <div>Task</div>
+    <div>
+      <div>Task</div>
+     
+      {list.length? list.map((item,index)=>{
+        return <p key={index}> time : {item.time} , title :{item.title}   ,description: {item.description}    <button type="" onClick={()=>{
+          handleDelete(index)
+        }} >delete</button> </p>
+      }):""}
+
+
+    </div>
   )
 }
 
